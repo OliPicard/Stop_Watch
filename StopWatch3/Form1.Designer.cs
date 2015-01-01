8af49096a61c23c45420f96509cca778d10ff40b
OliPicard <oliver@ufomail.co.uk>
2015-01-01 18:44:28 +0000

    minor change to formatting.

--- StopWatch3/Form1.Designer.cs
+++ StopWatch3/Form1.Designer.cs
@@ -79,6 +79,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Stop_Watch";
             this.ResumeLayout(false);
